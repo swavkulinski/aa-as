745cd5b2be94b50a680d19d244cd4465d37f2823
Swav Kulinski <swav.kulinski@gmail.com>
2013-05-28 11:27:30 +0100

    Template project for using AndroidAnnotations with Google Android Gradle plugin version 0.4

---
+++ Aa/src/main/java/org/labaa/aa/MainActivity.java
@@ -0,0 +1,18 @@
+package org.labaa.aa;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
